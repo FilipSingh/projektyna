73ab33ff396c6cf7562d660f00d7e0259cacf2af
FilipSingh <115579081+FilipSingh@users.noreply.github.com>
2022-10-12 20:29:44 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldSingh/HelloWorldSingh.java
@@ -0,0 +1,12 @@
+package pl.vistula.helloworldSingh;
+
+
+public class HelloWorldSingh
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World Singh");
+        System.out.println("good morning");
+        System.out.println("goodbye");
+        System.out.println("good afternoon");
+    }
+}
